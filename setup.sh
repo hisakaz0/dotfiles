8ae278bf729f0d999e02e1fa3ff95a7988ec5623
pinkienort <cantabilehisa@gmail.com>
2017-05-05 21:11:44 +0900

    fix bugs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd `dirname $0`
-pwd=`pwd`
+dotfiles_root=$(cd $0; pwd)
+dotfiles_root=${dotfiles_root#$HOME\/} # relative path
+list=`ls -a`
 ignore_list=( \
   "." \
   ".." \
@@ -10,14 +11,18 @@ ignore_list=( \
   "README.md" \
   "setup.sh" \
 )
-for file in `ls -a`
+cd $HOME
+for file in $list
 do
   ignore_flag=0
   for ignore in ${ignore_list[@]}
   do
     [ "$ignore" = "$file" ] && ignore_flag=1
   done
-  [ $ignore_flag -eq 1 ] && continue
-  cmd="ln -is $pwd/$file $HOME"
-  echo $cmd ; $cmd
+  if [ $ignore_flag -eq 1 ] ; then
+    echo ">> '$file' is ignored." 1>&2
+  else
+    cmd="ln -is $dotfiles_root/$file $HOME"
+    echo $cmd ; $cmd
+  fi
 done
