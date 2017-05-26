cbdcc6ea672274efe8f67db557ab0bbc221b2dd9
pinkienort <cantabilehisa@gmail.com>
2017-05-26 09:40:08 +0900

    link to the target with white-list

--- setup.sh
+++ setup.sh
@@ -1,28 +1,12 @@
 #!/bin/bash
-dotfiles_root=$(cd $0; pwd)
-dotfiles_root=${dotfiles_root#$HOME\/} # relative path
-list=`ls -a`
-ignore_list=( \
-  "." \
-  ".." \
-  ".git" \
-  ".DS_Store" \
-  ".gitignore" \
-  "README.md" \
-  "setup.sh" \
-)
+dotfiles_root=$(cd $(dirname $0) && pwd)
+list=(.bashrc .config .cshrc .cshrc.body .gitconfig .inputrc .tmux.conf .vim/after .vim/autoload .vim/plugin .vim/templates .vim/colors .vimrc)
 cd $HOME
-for file in $list
+for file in ${list[*]}
 do
-  ignore_flag=0
-  for ignore in ${ignore_list[@]}
-  do
-    [ "$ignore" = "$file" ] && ignore_flag=1
-  done
-  if [ $ignore_flag -eq 1 ] ; then
-    echo ">> '$file' is ignored." 1>&2
-  else
-    cmd="ln -is $dotfiles_root/$file $HOME"
-    echo $cmd ; $cmd
-  fi
+  src="$dotfiles_root/$file"
+  dict="$HOME/$file"
+  cmd="ln -sf $src $dict"
+
+  echo $cmd ; $cmd
 done
