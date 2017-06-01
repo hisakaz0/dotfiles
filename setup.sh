4e8cf1602fc5a114004586a35f8cc9503ac553b8
pinkienort <cantabilehisa@gmail.com>
2017-06-01 21:57:37 +0900

    add .screenrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dotfiles_root=$(cd $(dirname $0) && pwd)
-list=(.bashrc .config .cshrc .cshrc.body .gitconfig .inputrc .tmux.conf .vim/after .vim/autoload .vim/plugin .vim/templates .vim/colors .vimrc)
+list=(.bashrc .config .cshrc .cshrc.body .gitconfig .inputrc .tmux.conf .vim/after .vim/autoload .vim/plugin .vim/templates .vim/colors .vimrc .screenrc)
 cd $HOME
 for file in ${list[*]}
 do
