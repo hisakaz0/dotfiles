36aeed6dc3554d28d87ee8159c563d1e8f7aeb3e
hisakazu <cantabilehisa@gmail.com>
2025-10-14 00:00:17 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,8 @@ dotfiles_root=$(pwd)
 }
 
 : "main" && {
+	echo
+	echo "link dotfiles..."
   cd $HOME
   for file in ${list[*]}
   do
@@ -27,11 +29,9 @@ dotfiles_root=$(pwd)
   done
 }
 
-: "vim" && {
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh)"
-}
-
 : "brew" && {
+	echo
+	echo "brew install..."
   cd $dotfiles_root
   if type brew &>/dev/null ; then
     brew bundle
@@ -43,10 +43,12 @@ dotfiles_root=$(pwd)
 }
 
 : "mac" && {
+	echo
+	echo "mac setting..."
   # long pressでアクセント付き文字のポップアップを無効にする
   defaults write -g ApplePressAndHoldEnabled -bool false
 
-  echo "スクリーンショットの保存ディレクトリの変更"
+  echo "次を読んで、スクリーンショットの保存ディレクトリの変更してください"
   echo "https://www.reddit.com/r/macbookair/comments/1cg075e/how_can_i_change_the_default_folder_on_a_mac/"
 }
 
