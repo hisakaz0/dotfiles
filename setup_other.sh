985fcf72684bb89d13f78d7acad1c62dbed9f869
hisakazu <cantabilehisa@gmail.com>
2025-10-14 00:51:20 +0900

    update setup_other.sh

--- setup_other.sh
+++ setup_other.sh
@@ -5,7 +5,10 @@ set -eu
 : "other tools" && {
 	echo
 	echo ">>> install other tools..."
-	# On macOS and Linux.
+	# python uv
 	curl -LsSf https://astral.sh/uv/install.sh | sh
+	# vim plug
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
